92104ee31c9e7bec79c476e265aec80f348eeac7
BobTheWho777 <ruslankirillov82@gmail.com>
2025-03-13 13:54:27 +0300

    change repo

--- src/main/java/bip/online/biplio2023/repository/Book.java
+++ src/main/java/bip/online/biplio2023/repository/Book.java
@@ -3,5 +3,5 @@ package bip.online.biplio2023.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface Book extends JpaRepository<Book, Long> {
+public interface Book extends JpaRepository<bip.online.biplio2023.entity.Book, Long> {
 }
--- src/main/java/bip/online/biplio2023/repository/Genre.java
+++ src/main/java/bip/online/biplio2023/repository/Genre.java
@@ -2,5 +2,5 @@ package bip.online.biplio2023.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface Genre extends JpaRepository<Genre, Long> {
+public interface Genre extends JpaRepository<bip.online.biplio2023.entity.Genre, Long> {
 }
